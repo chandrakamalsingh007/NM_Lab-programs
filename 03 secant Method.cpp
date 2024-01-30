b70a2d4d6d53d2e2a5910a63a79a9208e7937ec4
chandrakamalsingh007 <149393399+chandrakamalsingh007@users.noreply.github.com>
2024-01-30 22:24:05 +0545

    secant method program

--- 03 secant Method.cpp
+++ 03 secant Method.cpp
@@ -10,4 +10,19 @@ int main(){
     cout<<"Enter initial guesses x0 and x1:"<<endl;
     cin>>x0;
     cin>>x1;
-    cout<<"\n Steps \t\tx0 \t\tx1 \t\t x2 \t\t f(x)\n"<<endl;
+    cout<<"\n Steps \t\tx0 \t\tx1 \t\t f0 \t\t f1 \t\t x2\n"<<endl;
+    do{
+        f0=f(x0);
+        f1=f(x1);
+
+        x2=((x0*f1)-(x1*f0))/(f1-f0);
+        f2=f(x2);
+        cout<<i<<" \t\t"<<x0<<" \t\t"<<x1<<" \t\t"<<f0<<" \t\t"<<f1<<" \t\t"<<x2<<endl;
+        x0=x1;
+        x1=x2;
+        i++;
+
+    }while(fabs(f2)>e);
+    cout<<"Root is "<<x2<<endl;
+    return 0;
+}
