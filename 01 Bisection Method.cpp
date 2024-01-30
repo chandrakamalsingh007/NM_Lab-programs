6c7f9abe8e3c694e7eb293f36abeae7130e490e8
chandrakamalsingh007 <149393399+chandrakamalsingh007@users.noreply.github.com>
2024-01-30 22:12:38 +0545

    Bisection Method program

--- 01 Bisection Method.cpp
+++ 01 Bisection Method.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    float a,b,f0,f1,f2,x;
+    float x0,x1,f0,f1,f2,x2;
     int step=1;
     up:
     cout<<setprecision(5)<<fixed;
@@ -13,20 +13,20 @@ int main(){
     cin>>b;
     cout<<"\n Steps \t\ta \t\tb \t\t x \t\t f(x)\n"<<endl;
     do{
-        f0=f(a);
-        f1=f(b);
-        if((f0*fb)>0){
+        f0=f(x0);
+        f1=f(x1);
+        if((f0*f1)>0){
         cout<<"Wrong guesses";
         goto up:
     }
-        x=(a+b)/2;
-        f2=f(x);
+        x2=(x0+x1)/2;
+        f2=f(x2);
         cout<<step<<" \t\t"<<a <<" \t\t"<<b<<" \t\t"<<x<<" \t\t"<<f2<<endl;
-        if(f0*f2<0){
-            b=x;
+        if((f0*f2)<0){
+            x1=x2;
             f1-f2;
         }else{
-            a=x;
+            x0=x2;
             f0=f2;
         }
         step++;
---
+++ 03 secant Method.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#define f(x) x*x*x-4*x-9
+#define e 0.0001
+using namespace std;
+
+int main(){
+    float x0,
+}
