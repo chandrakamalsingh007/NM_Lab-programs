872b978d2e29ff20b0a4a18fd747625f588cb890
chandrakamalsingh007 <149393399+chandrakamalsingh007@users.noreply.github.com>
2024-01-30 21:40:23 +0545

    Bisection Method program

---
+++ 01 Bisection Method.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define f(x) x*x*x-4*x-9
+#define e 0.0001
+using namespace std;
+
+int main(){
+    float a,b,f0,f1,f2,x;
+    int step=1;
+    up:
+    cout<<"Enter initial guesses a and b:"<<endl;
+    cin>>a;
+    cin>>b;
+    f0=f(a);
+    f1=f(b);
+    if((f0*fb)>0){
+        cout<<"Wrong guesses";
+        goto up:
+    }
+    cout<<"\n Steps \t\ta \t\tb \t\t x \t\t f(x)\n"<<endl;
+    do{
+        x=(a+b)/2;
+        f2=f(x);
+        cout<<step<<" \t\t"<<a <<" \t\t"<<b<<" \t\t"<<x<<" \t\t"<<f2<<endl;
+        if(f0*f2<0){
+            b=x;
+            f1-f2;
+        }else{
+            a=x;
+            f0=f2;
+        }
+        step++;
+
+    }while(fabs(f2)>e);
+    cout<<"Root is :"<<x<<endl;
+    return 0;
+
+}
+
+
