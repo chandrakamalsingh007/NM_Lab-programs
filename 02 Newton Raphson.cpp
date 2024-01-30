6971c192ff7854ae2992f5565d9742faa647a54a
chandrakamalsingh007 <149393399+chandrakamalsingh007@users.noreply.github.com>
2024-01-30 22:08:39 +0545

    Newton Raphson Method program

--- 02 Newton Raphson.cpp
+++ 02 Newton Raphson.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main(){
-    float x0,x,f0,g0,f1;
-    int i=1,N;
+    float x0,x1,f0,g0,f1;
+    int i=1,N;//iterations i
     cout<<setprecision(6)<<fixed;
     cout<<"Enter initial guess x0:";
     cin>>x0;
     cout<<"Enter maximum no. of iterations:";
     cin>>N;
-    
+    cout<<"\n iteration \t\t x0 \t\t x1"<<endl;
     do{
         f0=f(x0);
         g0=g(x0);
@@ -20,15 +20,19 @@ int main(){
         cout<<"Mathematical error!!"<<endl;
         exit(0);
     }
-        x=x0-(f0/g0);
-        f1=f(x);
-        cout<<"\n iteration \t\t x0 \t\t x"<<endl;
+        x1=x0-(f0/g0);
+        cout<<i<<"\t\t "<<x0<<"\t\t "<<x1<<endl;
+        f1=f(x1);
+        x0=x1;
         if(i>N){
             cout<<"Not convergent";
             exit(0);
         }
-        if()
+        i++;
+       
 
-    }
+    }while(fabs(f1)>e);
+    cout<<"Root is "<<x1;
+    return 0;
 
 }
